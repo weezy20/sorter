e7e5d1719ddc649a3482d6da7dbb2a9f314319f0
Abhishek Shah <abhishekshah3@gmail.com>
2021-07-07 17:53:08 +0530

    Bubble sort implemented

--- src/bubble.rs
+++ src/bubble.rs
@@ -3,7 +3,15 @@ use crate::{Ordering, Sorter};
 pub struct Bubble;
 
 impl<T: Ordering> Sorter<T> for Bubble {
+    /// Bubble the heavier elements towards the ends
     fn sort(slice: &mut [T]) {
-        
+        let len = slice.len();
+        for i in 0..len {
+            for j in 0..len - 1 {
+                if slice[j] > slice[i] {
+                    slice.swap(j, i);
+                }
+            }
+        }
     }
 }
